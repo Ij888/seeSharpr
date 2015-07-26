a435aab60deb5962f6e4a1991bf8386c8c35d901
Ijem <ij.ofili@gmail.com>
2015-07-26 04:12:28 +0100

    added base file

---
+++ app.cs
@@ -0,0 +1,9 @@
+//using System;	
+// System namespace similar to a package in Java
+public class HelloWorld {
+   public static void Main(){		
+   // the Main function starts with capital M
+   // and needs not to have the famous args []
+      Console.WriteLine("Hello World!");
+   }
+}
