b84ed3e07c6b5ed4edb63e2277819eb40707ce7b
Ijem <ij.ofili@gmail.com>
2015-12-14 14:46:13 +0000

    added entry
    comparing a basic Java and C# main class

--- main.cs
+++ main.cs
@@ -2,8 +2,25 @@ using System;
 // System namespace similar to a package in Java
 public class HelloWorld {
    public static void Main(){		
-   // the Main function starts with capital M
-   // and needs not to have the famous args []
+   // the Main function starts with capital M and needs not to have the famous args []
       Console.WriteLine("Hello World!");
    }
 }
+
+/*
+using System;
+	class Greet //public Greet{
+		static void main() //public static void Main(){
+		console.writeline ("some string value");
+	}
+}
+=========================================== 
+
+
+  class effendi{
+	public static void main(string[]args){
+		System.out.println("some string value")
+	}
+}
+
+*/
