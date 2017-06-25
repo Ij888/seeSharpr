6dc9cffb9fc65f4d9c80e07792e11e4ceb6a53f8
Ij888 <ij.ofili@gmail.com>
2017-06-25 07:07:48 +0100

    added entry

--- main.cs
+++ main.cs
@@ -1,9 +1,9 @@
 using System;	
 // System namespace similar to a package in Java
 public class HelloWorld {
-   public static void Main(){		
-   // the Main function starts with capital M and needs not to have the famous args []
-      Console.WriteLine("Hello World!");
+   public static void main(){
+        // the Main function starts with <willWork>capital M</willWork> and needs not to have the args angular brackets []
+        Console.WriteLine("Hello World!");
    }
 }
 
